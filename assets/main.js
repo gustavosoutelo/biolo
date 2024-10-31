16c7b6e71b09687f731356431ea52e5fce7163b9
gustavosoutelo <166401244+gustavosoutelo@users.noreply.github.com>
2024-10-31 12:06:49 -0300

    g

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,12 @@ function criarCartao(pergunta,resposta){
    let conteudo = document.getElementById("conteudo")
 let cartao = document.createElement("article")
 cartao.classList = 'cartao'
-cartao.innerHTML = 'testando o inner'
+
+cartao.innerHTML = `<div class="cartao--conteudo">
+<div class="cartao--pergunta"><p>pergunta card 1</p></div> 
+<div class="cartao--resposta"><p>reposta card 1</p></div>
+</div>`
+
 conteudo.appendChild(cartao)
 }
 criarCartao()
