b4d142f0189cc016b9178fc67d486afdf2fabb78
gustavosoutelo <166401244+gustavosoutelo@users.noreply.github.com>
2024-11-01 08:42:42 -0300

    uf

--- pergunta.js
+++ pergunta.js
@@ -3,10 +3,21 @@ criarCartao(
     "resposta 1"
 )
 criarCartao(
-    "pergunta 1" ,
-    "resposta 1"
+    "pergunta 2" ,
+    "resposta 2"
 )
 criarCartao(
-    "pergunta 1" ,
-    "resposta 1"
+    "pergunta 3" ,
+    "resposta 3"
+)criarCartao(
+    "pergunta 4" ,
+    "resposta 4"
+)
+criarCartao(
+    "pergunta 5" ,
+    "resposta 5"
+)
+criarCartao(
+    "pergunta 6" ,
+    "resposta 6"
 )
