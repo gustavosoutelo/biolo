d27a91c58b8e5eb0717cfaaa4a72d26c4f8b2002
gustavosoutelo <166401244+gustavosoutelo@users.noreply.github.com>
2024-11-01 08:26:08 -0300

    dn

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let cartao = document.createElement("article")
 cartao.classList = 'cartao'
 
 cartao.innerHTML = `<div class="cartao--conteudo">
-<div class="cartao--pergunta"><p>pergunta card 1</p></div> 
-<div class="cartao--resposta"><p>reposta card 1</p></div>
+<div class="cartao--pergunta"><p>$(pergunta card 1)</p></div> 
+<div class="cartao--resposta"><p>$(reposta card 1)</p></div>
 </div>`
 
 conteudo.appendChild(cartao)
