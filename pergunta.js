d7da9f28fa73a86ee5c099be95c44c251ef0d4a1
gustavosoutelo <166401244+gustavosoutelo@users.noreply.github.com>
2024-11-21 12:06:29 -0300

    8efh

--- pergunta.js
+++ pergunta.js
@@ -1,24 +1,24 @@
 criarCartao(
     "PARA QUE SERVE A GENETICA?" ,
-    ""
+    "A genética serve para entender como as características dos seres vivos são herdadas, variam e influenciam o desenvolvimento e as doenças."
 )
 criarCartao(
     "QUEM É CONSIDERADO MENDEL PARA A GENETICA?" ,
-    "resposta 2"
+    "Gregor Mendel é considerado o "pai da genética" devido às suas descobertas fundamentais sobre a hereditariedade, baseadas em experimentos com plantas de ervilha."
 )
 criarCartao(
     "POR QUE A ESCOLHA DE ESTUDAR ERVILHAS? " ,
-    "resposta 3"
+    "Mendel escolheu as ervilhas por terem características visíveis e distintas, facilitando a observação dos padrões de hereditariedade."
 )
 criarCartao(
     "O  QUE É UM GENE HETEROZIGOTO OU HOMOZIGOTO?" ,
-    "resposta 4"
+    "Um gene heterozigoto possui alelos diferentes para uma característica, enquanto um gene homozigoto tem alelos iguais para essa característica."
 )
 criarCartao(
-    "QUAL SIGNIFICADO DE UM ALELO RECESIVO OU DOMINANTE? " ,
-    "resposta 5"
+    "QUAL SIGNIFICADO DE UM ALELO RECESIVO OU DOMINANTE?" ,
+    "Um alelo dominante se expressa quando presente, enquanto um alelo recessivo só se manifesta quando está presente em dose dupla (homozigoto)."
 )
 criarCartao(
     "QUAL DIFERENÇA ENTRE A PRIMEIRA E A SEGUNDA LEI DE MENDEL?" ,
-    "resposta 6"
+    "A primeira lei trata da segregação dos alelos, e a segunda da distribuição independente deles."
 )
